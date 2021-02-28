5f4812dae271c84df92613c817812d8a448d5a3d
Daniel K. Morita <daniel.morita@sotran.com.br>
2021-02-28 20:04:40 -0300

    final

---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import styles from "../styles/components/Profile.module.css";
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
+  return (
+    <div className={styles.profileContainer}>
+      <img
+        src="https://avatars.githubusercontent.com/u/2254731?s=460&u=4fcc8ca9672eeb41ea800271831b7c687bc17054&v=4"
+        alt="Diego Fernandes"
+      />
+      <div>
+        <strong>Diego Fernandes</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
